6f04acd73a73b80fbe2c722302f784d0bc093ab0
brian-ambani <brainambani1@gmail.com>
2023-12-24 13:55:55 +0300

    updated the GenreMovieLits

---
+++ hulu-clone/src/components/GenreMovieList.tsx
@@ -0,0 +1,27 @@
+import { IoChevronForwardSharp } from "react-icons/io5";
+import GenreList from "../constant/GenreList";
+
+const GenreMovieList = () => {
+  return (
+    <div className="p-5 px-10 md:px-20">
+      {GenreList.genre.map(
+        (item: any, index) =>
+          index <= 4 && (
+            <div className="text-[23px] text-white font-bold">
+              <h2 key={item.id}>
+                {item.name}{" "}
+                <span
+                  className="font-normal text-[16px] cursor-pointer
+            text-gray-400 float-right flex"
+                >
+                  VIEW ALL <IoChevronForwardSharp className="text-white ml-1" />{" "}
+                </span>
+              </h2>
+            </div>
+          )
+      )}
+    </div>
+  );
+};
+
+export default GenreMovieList;
