e4804632e8f740bcd70256d9b3621392e557bebf
brian-ambani <brainambani1@gmail.com>
2023-12-24 03:29:04 +0300

    updated the responsive nav for all devices

--- hulu-clone/src/components/Header.tsx
+++ hulu-clone/src/components/Header.tsx
@@ -1,6 +1,14 @@
-import { IoSearch } from "react-icons/io5";
+import { useState } from "react";
+
+import {
+  IoChevronDownOutline,
+  IoChevronUpOutline,
+  IoSearch,
+} from "react-icons/io5";
 import logo from "../assets/images/logo.png";
+
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
   const menu = [
     {
       id: 1,
@@ -31,18 +39,52 @@ const Header = () => {
   return (
     <div className="flex justify-between items-center p-4 px-10">
       <img src={logo} className="w-[65px]" alt="Hulu Logo" />
-      <ul className="flex gap-8">
+      <ul className="hidden md:flex gap-8">
         {menu.map((item) => (
           <li
             key={item.id}
-            className="text-gray-400 text-[18px] font-medium cursor-pointer hover:bg-gray-700 
-            hover:text-white px-3 py-1 
+            className="text-gray-400 text-[18px]
+            font-medium cursor-pointer hover:bg-gray-700 
+            hover:text-white px-3 pb-2 py-1 
             rounded-md transition-all duration-500 ease-in-out"
           >
             {item.name}
           </li>
         ))}
       </ul>
+      <div className="md:hidden ">
+        <h2
+          className="text-white font-medium flex items-center gap-2 px-3 py-2 pb-2
+                bg-gray-700 rounded-md cursor-pointer"
+          onClick={() => setToggle(!toggle)}
+        >
+          Home
+          {!toggle ? (
+            <IoChevronDownOutline className="mt-1" />
+          ) : (
+            <IoChevronUpOutline className="mt-1" />
+          )}
+        </h2>
+        {toggle ? (
+          <ul
+            className="absolute bg-gray-700
+          w-[200px] text-center mt-3 p-3 left-0 right-0 mx-auto rounded-md"
+          >
+            {menu.map((item) => (
+              <li
+                key={item.id}
+                className="text-gray-400 text-[18px]
+                font-medium cursor-pointer mb-3 mt-2 
+                hover:bg-gray-600 hover:text-white 
+                px-3 py-1 rounded-md transition-all duration-500"
+              >
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
+
       <div className="flex gap-10">
         <IoSearch
           className="text-[35px] text-gray-30
