77e299af993973d8b4894c35f47d24cd5dad084b
brian-ambani <brainambani1@gmail.com>
2023-12-25 03:53:52 +0300

    pdated the movielist

--- hulu-clone/src/components/MovieList.tsx
+++ hulu-clone/src/components/MovieList.tsx
@@ -1,31 +1,53 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import GlobalApi from "../services/GlobalApi";
 import MovieCard from "./MovieCard";
 
+import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
+
 const MovieList = (genreId: any) => {
+  const elementref = useRef<any>();
+
   const [movieList, setMovieList] = useState<any>([]);
 
   useEffect(() => {
     GenreMovieListByGenreId();
   }, []);
 
+  const slideRight = () => {
+    elementref.current.scrollLeft += 350;
+  };
+
+  const slideLeft = () => {
+    elementref.current.scrollLeft -= 350;
+  };
+
   const GenreMovieListByGenreId = () => {
     GlobalApi.getMoviesByGenreId(genreId.genreId).then((res: any) => {
-      console.log(res.data.results);
       setMovieList(res.data.results);
     });
   };
 
   return (
-    <div>
+    <div className="flex items-center ">
+      <IoChevronBackOutline
+        className="md:block text-[40px] z-10 text-white bg-black p-2
+      cursor-pointer rounded-full mb-[120px]"
+        onClick={() => slideLeft()}
+      />
       <div
         className="w-full overflow-scroll scroll-smooth overflow-x-auto
-      whitespace-nowrap scrollbar-hide mb-16"
+      whitespace-nowrap scrollbar-hide mb-16 ml-[-20px] mr-[-20px]"
+        ref={elementref}
       >
         {movieList.map(
           (item: any, index: any) => index < 7 && <MovieCard movie={item} />
         )}
       </div>
+      <IoChevronForwardOutline
+        className="md:block text-[40px] text-white bg-black p-2
+      cursor-pointer rounded-full mb-[120px] z-10"
+        onClick={() => slideRight()}
+      />
     </div>
   );
 };
